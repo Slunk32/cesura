e00ec6b4decc3462c02b87c4648193704a0eb76b
Russell Salerno <russell.j.salerno@gmail.com>
2016-06-20 23:39:47 -0700

    updated: added search for related artist that will give an object with related artist name, images, id, and toptrackpreview url

--- public/js/results.js
+++ public/js/results.js
@@ -1,18 +1,18 @@
-"use strict";
+"use strict"
 
 function play(uri) {
-	var player = document.getElementsByClassName('player')[0];
-	player.src = '';
-	player.src = uri;
+    var player = document.getElementsByClassName('player')[0]
+    player.src = ''
+    player.src = uri
 }
 
 var results = {
-	onDomLoaded: function() {
-		var playLinks = Array.from(document.getElementsByClassName('track'));
-		playLinks.forEach(function(playLink) {
-			playLink.addEventListener('click', play.bind(null, playLink.dataset.previewUrl))
-		});
-	}
+    onDomLoaded: function() {
+        var playLinks = Array.from(document.getElementsByClassName('track'))
+        playLinks.forEach(function(playLink) {
+            playLink.addEventListener('click', play.bind(null, playLink.dataset.previewUrl))
+        })
+    }
 }
 
-module.exports = results;
+module.exports = results
