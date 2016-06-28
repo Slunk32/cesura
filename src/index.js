6821d0c1b910f8e0e000aeba773e2a8f164a3c17
Andrew Archer <andrew.m.archer@gmail.com>
2016-06-27 19:21:04 -0700

    http and https

--- src/index.js
+++ src/index.js
@@ -143,14 +143,13 @@ app.post('/remove-playlist-items', function(req, res) {
 });
 
 app.get('/login', function(req, res) {
-    const host = req.headers.host;
-    const prefix = host.indexOf('local') > -1 ? 'http://' : 'https://';
+    { protocol, host } = req.headers.host;
 
 	res.redirect('https://accounts.spotify.com/authorize' +
 	  '?response_type=token' +
 	  '&client_id=' + SPOTIFY_ID +
 	  '&scope=' + encodeURIComponent(SCOPES.join(' ')) +
-	  '&redirect_uri=' + encodeURIComponent(prefix + host + REDIRECT_URI));
+	  '&redirect_uri=' + encodeURIComponent(protocol + host + REDIRECT_URI));
 });
 
 app.get('/authenticate', function(req, res) {
